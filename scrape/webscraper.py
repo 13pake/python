8522339b108dde6730902570c196139b8e7d955f
Pake <13pake@gmail.com>
2016-08-18 08:02:55 -1000

    Last update from 8am

--- scrape/webscraper.py
+++ scrape/webscraper.py
@@ -10,6 +10,8 @@ from bs4 import BeautifulSoup
 url_path = 'http://www.toyota.com/prius/2017/features/exterior/1223/1224/1225/1226'
 page = urllib2.urlopen(url_path)
 
-soup = BeautifulSoup(page)
+soup = BeautifulSoup(page, "html.parser") # used 2nd thing to get rid of error
 
-print soup.prettify()
+soup.prettify()
+
+print soup.find_all('tr', class_='tcom-datamatrix-row')
