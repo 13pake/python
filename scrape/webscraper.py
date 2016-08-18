2f9fa09e39afa24541e8df7af760fbe4d33a3486
Trevor Roberts <trinaroberts@Pake-MacBook-Pro.local>
2016-08-18 07:51:25 -1000

    Start scraping\!

---
+++ scrape/webscraper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# WEB SCRAPER
+# https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/
+
+import sys
+import urllib2
+from bs4 import BeautifulSoup
+
+url_path = 'http://www.toyota.com/prius/2017/features/exterior/1223/1224/1225/1226'
+page = urllib2.urlopen(url_path)
+
+soup = BeautifulSoup(page)
+
+print soup.prettify()
